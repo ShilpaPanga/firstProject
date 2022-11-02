46b86f1711de531375c239a8a8a72b2b4d070678
ShilpaPanga <panga.shilpa@gmail.com>
2022-11-01 20:41:44 -0400

    shilpa sout

--- src/com/pragra/day8/SwitchDemo.java
+++ src/com/pragra/day8/SwitchDemo.java
@@ -6,5 +6,6 @@ public class SwitchDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("new feature");
+        System.out.println("second feature");
     }
 }
